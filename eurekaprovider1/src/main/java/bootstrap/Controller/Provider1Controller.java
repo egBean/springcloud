dc70f245cc6765c943e329ef30bcf09febffc379
egbean <1358404767@qq.com>
2019-09-24 16:01:53 +0800

    增加一个获取注册中心所有实例的接口。增加classes文件忽略。

--- eurekaprovider1/src/main/java/bootstrap/Controller/Provider1Controller.java
+++ eurekaprovider1/src/main/java/bootstrap/Controller/Provider1Controller.java
@@ -2,17 +2,26 @@ package bootstrap.Controller;
 
 import bootstrap.entity.People;
 import bootstrap.repository.PeopleRepository;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
+import com.netflix.discovery.shared.Applications;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @RestController
 public class Provider1Controller {
 
     @Value("${server.port}")
     private String port;
+
+
+    @Autowired
+    private EurekaClient eurekaClient;
 
     @Autowired
     private PeopleRepository peopleRepositoryImpl;
@@ -45,5 +54,16 @@ public class Provider1Controller {
     @GetMapping("/port")
     public String getPort(){
         return this.port;
+    }
+
+    @RequestMapping("/get")
+    public List<String> getAppNames(){
+        List<String> result = new ArrayList<>();
+        Applications applications = eurekaClient.getApplications();
+        List<Application> registeredApplications = applications.getRegisteredApplications();
+        for(Application application : registeredApplications){
+            result.add(application.getName());
+        }
+        return result;
     }
 }
